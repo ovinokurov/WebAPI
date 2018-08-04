860e2fd3efd3db496129ff67a2a83b369f7decc0
LAPTOP-G4P04BKR\olegv <olegvinokurov@hotmail.com>
2018-08-03 19:21:34 -0700

    Add project files.

---
+++ WebApi/Controllers/DataController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : Controller
+    {
+        private readonly DataContext _context;
+
+        public DataController(DataContext context)
+        {
+            _context = context;
+
+            if (_context.DataItems.Count() == 0)
+            {
+                _context.DataItems.Add(new DataItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<DataItem>> GetAll()
+        {
+            return _context.DataItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetData")]
+        public ActionResult<DataItem> GetById(long id)
+        {
+            var item = _context.DataItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpPost]
+        public IActionResult Create(DataItem item)
+        {
+            _context.DataItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetData", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, DataItem item)
+        {
+            var todo = _context.DataItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.DataItems.Update(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.DataItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.DataItems.Remove(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ WebApi/Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+
+
+namespace WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Image")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IHostingEnvironment _environment;
+        public ImageController(IHostingEnvironment environment)
+        {
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        // POST: api/Image
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+            if (string.IsNullOrWhiteSpace(_environment.WebRootPath))
+            {
+                _environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            }
+
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+
+            if (file.Length > 0)
+            {
+                using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+        }
+
+        // GET: api/Image/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+    }
+}
